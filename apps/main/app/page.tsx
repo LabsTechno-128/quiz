a07213c9385e6360aacad8f88ff14a90fb7aabf7
Abdul Malek <abdulmalek.swe.585@gmail.com>
2026-04-14 11:28:04 +0600

    logo added

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
 
   return (
     <div className="max-w-[1440px] mx-auto">
-      <h1>i want to remove the boss system</h1>
+      <h1>this is best line never ever</h1>
       <HeroSection></HeroSection>
 
       {/* Categories Section */}
