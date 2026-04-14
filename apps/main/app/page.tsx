00585392ac1196aa45bdd08c06f3fd9844dac697
Abdul Malek <abdulmalek.swe.585@gmail.com>
2026-04-14 10:04:09 +0600

    fix solve issue

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
 
   return (
     <div className="max-w-[1440px] mx-auto">
-      <h1>shariar is my frsdfdsiend</h1>
+      <h1>shariar is my sdfdsfdsffrsdfdsiend</h1>
       <HeroSection></HeroSection>
 
       {/* Categories Section */}
